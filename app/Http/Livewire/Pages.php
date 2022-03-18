2f50120afe402889f314147b3965c0be19ab2972
BadreddineZatout <hb_zatout@esi.dz>
2022-03-18 17:31:35 +0100

    init project

---
+++ app/Http/Livewire/Pages.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Pages extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages');
+    }
+}
---
+++ database/migrations/2022_03_17_141900_create_pages_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pages', function (Blueprint $table) {
+            $table->id();
+            $table->string('text')->nullable();
+            $table->string('slug')->nullable();
+            $table->longText('content')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pages');
+    }
+};
